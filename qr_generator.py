97128458bbab24c0bf125377fb1beba49be63f6b
srini1183 <50111526+srini1183@users.noreply.github.com>
2020-07-19 12:04:36 +0530

    Update qr_generator.py

--- qr_generator.py
+++ qr_generator.py
@@ -1,5 +1,5 @@
 import pyqrcode # to create qrcode
 import png # to save qrcode in png format
 
-url = pyqrcode.create('https://4f4eb16fdb27.ngrok.io/download') # provide data to reside inside the qrcode as argument
+url = pyqrcode.create('https://4f4eb16fdb27.ngrok.io/download') # provide data to reside inside the qrcode as argument.In my case, the data is an url.
 url.png('My_image_qr_code.png',scale = 8) #save the qrcode as png file
