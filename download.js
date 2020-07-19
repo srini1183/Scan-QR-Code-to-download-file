aee5de50cb6393a226d19d20f139dea84956b655
srini1183 <50111526+srini1183@users.noreply.github.com>
2020-07-19 12:06:04 +0530

    Update download.js

--- download.js
+++ download.js
@@ -1,15 +1,14 @@
-const express = require('express')
+const express = require('express') 
 const path = require('path')
 const app = express()
 
 
-const file = path.join(__dirname,'srini.jpg')
+const file = path.join(__dirname,'srini.jpg') //Path to the file
 
 app.get('/download',(req,res)=>{
-    
-    res.download(file)
+   res.download(file) 
 })
 
 app.listen(3000,()=>{
-    console.log('Server running..')
+    console.log('Server running..') //Server running on port 3000
 })
